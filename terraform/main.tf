15b01562cb92f6170a6d673d7f3f674329c54f6a
Kosuke-Kg <kosuke.kg1127@gmail.com>
2025-05-25 06:07:02 +0000

    add:subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 
   tags = {
-    Name                     = "${local.project_name}-vpc"
-    "${local.project_tag}"   = local.project_name
+    Name                   = "${local.project_name}-vpc"
+    "${local.project_tag}" = local.project_name
   }
 }
 
@@ -20,7 +20,80 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name                     = "${local.project_name}-igw"
-    "${local.project_tag}"   = local.project_name
+    Name                   = "${local.project_name}-igw"
+    "${local.project_tag}" = local.project_name
+  }
+}
+
+resource "aws_subnet" "public-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                   = "${local.project_name}-public-1a"
+    "${local.project_tag}" = local.project_name
+  }
+}
+
+resource "aws_subnet" "public-1c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                   = "${local.project_name}-public-1c"
+    "${local.project_tag}" = local.project_name
+  }
+}
+
+resource "aws_subnet" "public-1d" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-northeast-1d"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                   = "${local.project_name}-public-1d"
+    "${local.project_tag}" = local.project_name
+  }
+}
+
+# プライベートサブネット
+resource "aws_subnet" "private_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name                   = "${local.project_name}-private-1a"
+    "${local.project_tag}" = local.project_name
+    Type                   = "private"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name                   = "${local.project_name}-private-1c"
+    "${local.project_tag}" = local.project_name
+    Type                   = "private"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.13.0/24"
+  availability_zone = "ap-northeast-1d"
+
+  tags = {
+    Name                   = "${local.project_name}-private-1d"
+    "${local.project_tag}" = local.project_name
+    Type                   = "private"
   }
 }
